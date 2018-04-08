211e8ca66c7b4a1977b9f5e8bb3e2f57050c01a2
Dawid Boksa <boksa.dawid@interia.pl>
2018-04-08 22:29:21 +0200

    finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '') #Returns page title 
+    base_title = "railstutorial"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " - " + base_title
+      end
+    end
 end
