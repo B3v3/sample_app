79db1936b3cb5212f45711d9de424960865ef6a9
Dawid Boksa <boksa.dawid@interia.pl>
2018-04-13 03:16:51 +0200

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  #returns the gravatar for user
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+end
+    def number_to_word(int) #for error messages
+      numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+      number = numbers[int - 1]
+      return number
+end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+
+      post signup_path, params: { user: { name: "",
+                                         email: "invalid@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar"}}
+      end
+
+      assert_template "users/new"
+
+      assert_select 'div.field_with_errors', count: 8
+      assert_select 'div#error_explanation'
+      end
+
+      test "successful signup" do
+        get signup_path
+        assert_select 'form[action="/signup"]'
+          assert_difference 'User.count', 1 do
+
+            post signup_path, params: { user: { name: "bizbuzz",
+                                               email: "valid@is.valid",
+                                               password: "foobar",
+                                               password_confirmation: "foobar"}}
+            end
+            follow_redirect!
+            assert_template "users/show"
+            assert_not flash.empty?
+      end
+end
